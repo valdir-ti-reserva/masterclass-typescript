929fda3176f4e6644c201b489db31b033eb14e57
Valdir Silva <valdir.ti@gmail.com>
2020-12-10 14:43:40 -0300

    feat: create interface to UserController

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import EmailService from '../services/EmailService';
 
 const users = [
   {
@@ -12,7 +13,24 @@ const users = [
 ]
 
 export default {
-  async index (req: Request, res: Response) {    
+  
+  async index (req: Request, res: Response) {
     return res.status(200).json(users);
+  },
+
+  async create(req: Request, res: Response) {
+    const emailService = new EmailService();
+    emailService.sendMail({
+      to: { 
+        name: 'Lila', 
+        email: 'lila@mail.com' 
+      }, 
+      message: { 
+        subject: 'Bem-vindo ao sistema', 
+        body: 'Seja bem-vindo' 
+      }
+    });
+
+    res.status(200).json({ data: 'Success' });
   }
-}
+};
